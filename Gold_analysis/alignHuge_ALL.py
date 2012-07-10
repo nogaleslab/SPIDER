41d438b4e9b1f9b03f192d47a936f291a0b00a02
Michael Cianfrocco <michael.a.cianfrocco@gmail.com>
2012-07-10 13:12:40 -0700

    Added filtering before focal pair alignment

--- Gold_analysis/alignHuge_ALL.py
+++ Gold_analysis/alignHuge_ALL.py
@@ -12,7 +12,12 @@ for en in list:
 	
 	ef = re.sub("en","ef",en)
 	efAli = re.sub("en","efAli",en)
-	
-	cmd="alignhuge %s %s %s" %(ef,en,efAli)
-	#system("proc2d %s.xmp %s.img"%(file,file))
+
+	cmd='proc2d %s %s_filt.mrc apix=1.501 lp=20' %(ef,ef[:-4])
+	subprocess.Popen(cmd,shell=True).wait()
+
+	cmd="alignhuge %s_filt.mrc %s %s" %(ef[:-4],en,efAli)
+	subprocess.Popen(cmd,shell=True).wait()
+
+	cmd='rm %s_filt.mrc %s' %(ef[:-4],efAli)
 	subprocess.Popen(cmd,shell=True).wait()
